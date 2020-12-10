7207cb25677ebf1faf15c23b7703987efc228831
PupRiku <chris.diorio12@gmail.com>
2020-12-10 13:09:55 -0600

    Home page: Icon interactivity implemented

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,39 +8,54 @@ const skillImg = document.getElementById('skill-img');
 const projectsImg = document.getElementById('projects-img');
 const contactImg = document.getElementById('contact-img');
 
-const menuEnter = (textID) => {
-  switch(textID.id){
-    case 'about-link':
-      break;
-    case 'skills-link':
-      break;
-    case 'projects-link':
-      break;
-    case 'contact-link':
-      break;
-  }
-}
-const menuExit = (textID) => {
-  switch(textID.id){
-    case 'about-link':
-      break;
-    case 'skills-link':
-      break;
-    case 'projects-link':
-      break;
-    case 'contact-link':
-      break;
-  }
-}
-
-aboutText.onmouseenter = menuEnter(aboutText);
-aboutText.onmouseout = menuExit(aboutText);
-skillText.onmouseenter = menuEnter(skillText);
-skillText.onmouseout = menuExit(skillText);
-projectsText.onmouseenter = menuEnter(projectsText);
-projectsText.onmouseout = menuExit(projectsText);
-contactText.onmouseenter = menuEnter(contactText);
-contactText.onmouseout = menuExit(contactText);
+aboutText.onmouseenter = function() {
+  aboutImg.style.width = '100px';
+  aboutImg.style.height = 'auto';
+  aboutImg.style.opacity = '1';
+  aboutImg.style.transitionDuration = '2s';
+};
+aboutText.onmouseleave = function() {
+  aboutImg.style.width = '1px';
+  aboutImg.style.height = 'auto';
+  aboutImg.style.opacity = '0';
+  aboutImg.style.transitionDuration = '2s';
+};
+skillText.onmouseenter = function() {
+  skillImg.style.width = '100px';
+  skillImg.style.height = 'auto';
+  skillImg.style.opacity = '1';
+  skillImg.style.transitionDuration = '2s';
+};
+skillText.onmouseleave = function() {
+  skillImg.style.width = '1px';
+  skillImg.style.height = 'auto';
+  skillImg.style.opacity = '0';
+  skillImg.style.transitionDuration = '2s';
+};
+projectsText.onmouseenter = function() {
+  projectsImg.style.width = '100px';
+  projectsImg.style.height = 'auto';
+  projectsImg.style.opacity = '1';
+  projectsImg.style.transitionDuration = '2s';
+};
+projectsText.onmouseleave = function() {
+  projectsImg.style.width = '1px';
+  projectsImg.style.height = 'auto';
+  projectsImg.style.opacity = '0';
+  projectsImg.style.transitionDuration = '2s';
+};
+contactText.onmouseenter = function() {
+  contactImg.style.width = '100px';
+  contactImg.style.height = 'auto';
+  contactImg.style.opacity = '1';
+  contactImg.style.transitionDuration = '2s';
+};
+contactText.onmouseleave = function() {
+  contactImg.style.width = '1px';
+  contactImg.style.height = 'auto';
+  contactImg.style.opacity = '0';
+  contactImg.style.transitionDuration = '2s';
+};
 
 
 const source = document.getElementById('popup').innerHTML;
